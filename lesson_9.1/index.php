df781d8b93697fcc7dfb3aa3406b1cefc799362c
Aleksey <alassstttar@gmail.com>
2024-02-20 22:22:07 +0200

    Lesson 9.1

---
+++ lesson_9.1/index.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types = 1);
+
+require 'BankAccount.php';
+
+$bankAccount = new BankAccount(111,9999);
+
+$secondBankAccount = new BankAccount(222,0);
+
+
+$secondBankAccount->replenishment($bankAccount->getBalance());
+
+$bankAccount->withdraw($bankAccount->getBalance());
+
+
+
+echo $bankAccount->getAccountNumber() . '  ' . $bankAccount->getBalance() . PHP_EOL;
+
+echo $secondBankAccount->getAccountNumber() . '  ' . $secondBankAccount->getBalance() . PHP_EOL;
+
+
